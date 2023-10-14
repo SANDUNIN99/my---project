f64fa2a6f75f19fe24116a4ec75ed868bf4fc763
FERNANDO K.B.R.N <kbrnfernando96@outlook.com>
2023-10-14 23:04:09 +0530

    Made simple change

--- simple_tax_calculator.py
+++ simple_tax_calculator.py
@@ -1,11 +1,12 @@
 def calculate_income_tax(income):
-    if income <= 50000:
+    if income <= 75000:
         tax = 0
     elif income <= 200000:
-        tax = 0.20 * (income - 50000)
+        tax = 0.20 * (income - 75000)
     else:
-        tax = 0.30 * (income - 50000)
+        tax = 0.30 * (income - 75000)
     return tax
+
 
 # Get user input for income
 income = float(input("Please enter your income: $"))
