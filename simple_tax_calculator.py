bdf99463695aeef45b648a2b03b183006b085e4a
SANDUNIN99 <saduni.nn@gmail.com>
2023-10-14 22:32:53 +0530

    Updated the python code

--- simple_tax_calculator.py
+++ simple_tax_calculator.py
@@ -1,14 +1,14 @@
 def calculate_income_tax(income):
-    if income <= 100000:
+    if income <= 50000:
         tax = 0
-    elif income <= 500000:
-        tax = 0.20 * (income - 100000)
+    elif income <= 200000:
+        tax = 0.20 * (income - 50000)
     else:
-        tax = 0.30 * (income - 500000)
+        tax = 0.30 * (income - 50000)
     return tax
 
 # Get user input for income
-income = float(input("Enter your income: $"))
+income = float(input("Please enter your income: $"))
 
 # Calculate the income tax
 tax = calculate_income_tax(income)
